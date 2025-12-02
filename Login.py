f6d316f6faed59c94e6e11fed99a928f05899f19
rickegss <ricksilvagomes1@gmail.com>
2025-12-02 11:05:06 -0300

    refactor: renomeação do arquivo em ui_config  pra general_config

--- Login.py
+++ Login.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from src.core.app import main
-from src.ui_config.login_config import setup_page_config, apply_login_styles, render_footer
+from ui_config.general_config import setup_page_config, apply_login_styles, render_footer
 
 setup_page_config()
 
